43367630a6c0e4268ab83d48b4c0ec46187d253c
Antonio Guedes <gguedesantonio@gmail.com>
2020-07-27 13:06:26 +1000

    Object destructuring added to app.js

--- app.js
+++ app.js
@@ -13,11 +13,11 @@ const render = require("./lib/htmlRenderer");
 function generateRoster() {
     // Synchronously check if a file||directory exists.
     if (!fs.existsSync(OUTPUT_DIR)) {
-      fs.mkdirSync(OUTPUT_DIR);
+        fs.mkdirSync(OUTPUT_DIR);
     }
     fs.writeFileSync(outputPath, render(team), "utf-8");
-  }
-  
+}
+
 
 // An array containing all members information
 
@@ -26,45 +26,45 @@ const team = [];
 // prompt user about manager
 
 function createManager() {
-    return inquirer.prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "What is your manager's name?"
-      },
-      {
-        type: "input",
-        name: "id",
-        message: "What is your manager's ID?",
-        validate: (value) => !isNaN(value) || "Please enter a number.",
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "What is your manager's email?"
-      },
-      {
-        type: "input",
-        name: "officeNumber",
-        message: "What is your manager's office number?",
-        validate: (value) => !isNaN(value) || "Please enter a number.",
-      }
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your manager's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your manager's ID?",
+            validate: (value) => !isNaN(value) || "Please enter a number.",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your manager's email?"
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is your manager's office number?",
+            validate: (value) => !isNaN(value) || "Please enter a number.",
+        }
     ])
-  .then((res) => {
-    const manager = new Manager(
-      res.name,
-      res.id,
-      res.email,
-      res.officeNumber
-    );
-    team.push(manager);
-    addMember();
-  });
+        .then(({ name, id, email, officeNumber }) => {
+            const manager = new Manager(
+                name,
+                id,
+                email,
+                officeNumber
+            );
+            team.push(manager);
+            addMember();
+        });
 }
 
 // prompt user about adding a member
 function addMember() {
-    return inquirer.prompt([
+    inquirer.prompt([
         {
             type: "list",
             name: "memberType",
@@ -76,95 +76,94 @@ function addMember() {
             ]
         }
     ])
-    .then((res) => {
-        switch (res.memberType) {
-            case "Engineer":
-              createEngineer();
-              break;
-            case "Intern":
-              createIntern();
-              break;
-            default:
-              generateRoster();
-          }
+        .then(({ memberType }) => {
+            switch (memberType) {
+                case "Engineer":
+                    createEngineer();
+                    break;
+                case "Intern":
+                    createIntern();
+                    break;
+                default:
+                    generateRoster();
+            }
         }
-
-    );
+        );
 }
 
 // prompt user about new engineer
 function createEngineer() {
     return inquirer.prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "What is your engineer's name?"
-      },
-      {
-        type: "input",
-        name: "id",
-        message: "What is your engineer's ID?",
-        validate: (value) => !isNaN(value) || "Please enter a number.",
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "What is your engineer's email?"
-      },
-      {
-        type: "input",
-        name: "github",
-        message: "What is your engineer's GitHub username?",
-      }
+        {
+            type: "input",
+            name: "name",
+            message: "What is your engineer's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your engineer's ID?",
+            validate: (value) => !isNaN(value) || "Please enter a number.",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your engineer's email?"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is your engineer's GitHub username?",
+        }
     ])
-  .then((res) => {
-    const engineer = new Engineer(
-      res.name,
-      res.id,
-      res.email,
-      res.github
-    );
-    team.push(engineer);
-    addMember();
-  });
-}  
+        .then(({ name, id, email, github }) => {
+            const engineer = new Engineer(
+                name,
+                id,
+                email,
+                github
+            );
+            team.push(engineer);
+            addMember();
+        });
+}
 
 // prompt user about new intern
 function createIntern() {
     return inquirer.prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "What is your intern's name?"
-      },
-      {
-        type: "input",
-        name: "id",
-        message: "What is your intern's ID?",
-        validate: (value) => !isNaN(value) || "Please enter a number.",
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "What is your intern's email?"
-      },
-      {
-        type: "input",
-        name: "school",
-        message: "What is your intern's school?",
-      }
+        {
+            type: "input",
+            name: "name",
+            message: "What is your intern's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your intern's ID?",
+            validate: (value) => !isNaN(value) || "Please enter a number.",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your intern's email?"
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "What is your intern's school?",
+        }
     ])
-  .then((res) => {
-    const intern = new Intern(
-      res.name,
-      res.id,
-      res.email,
-      res.school
-    );
-    team.push(intern);
-    addMember();
-  });
-}  
+        .then(({ name, id, email, school }) => {
+            const intern = new Intern(
+                name,
+                id,
+                email,
+                school
+            );
+            team.push(intern);
+            addMember();
+        });
+}
 
-  createManager();
+createManager();
 
